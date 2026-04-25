975f9fe82a2fb3d41426ac5fc70a6cf1d12970ab
19yusufozcan <19yusufozcan@gmail.com>
2026-04-25 02:20:20 -0400

    feat: add EditProfile ViewModel for user profile editing

---
+++ Models/EditProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightManagementWeb.Models;
+
+public class EditProfile
+{
+    public string FirstName { get; set;}
+    public string LastName { get; set;}
+    public string Email { get; set;}
+    public string PhoneNumber { get; set;}
+    public DateOnly DateOfBirth {get; set;}
+    public string? ProfilePictureUrl { get; set; }
+}
