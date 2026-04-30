e128a0eec025ec89293a155f1cfa1e398afba48c
19yusufozcan <19yusufozcan@gmail.com>
2026-04-30 04:33:24 -0400

    feat: create ArchivedPurchase model

--- Models/ArchivedPurchase.cs
+++ Models/ArchivedPurchase.cs
@@ -1,6 +1,24 @@
-﻿namespace FlightManagementWeb.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightManagementWeb.Models;
 
 public class ArchivedPurchase
 {
+    public Flight? Flight { get; set; }
+    
+    [Key]
+    public int ArchivedPurchaseId {get; set;}
+    public int OriginalPurchaseNumber {get; set;}
+    public DateTime ArchivedDate {get; set;} 
+    
+    public int FlightId {get; set;}
+        
+    public string? UserId {get; set;}
+    
+    public string FirstName {get; set;}
+    
+    public string LastName {get; set;}
+    
+    public string Email {get; set;}
     
 }
