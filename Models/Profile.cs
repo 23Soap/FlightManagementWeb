a57f5bad19387c37ea6a2aa9ecbeb7291d1318b8
19yusufozcan <19yusufozcan@gmail.com>
2026-04-30 04:54:23 -0400

    refactor: change PreviousFlight type to ArchivedPurchase

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -5,7 +5,7 @@ public class Profile
     // To display Users Informations
     public ApplicationUser? User { get; set; }
     // To display Previous bought flights
-    public List<Purchase>? PreviousFlight { get; set; }
+    public List<ArchivedPurchase>? PreviousFlight { get; set; }
     // To display Next flight
     public List<Purchase>? NextFlight { get; set; }
     // To display Todays Flight
